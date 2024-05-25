b3811f5f24b4d82f329b76e41b2d9d59cc511483
haci1995 <hajiaghaaa@code.edu.az>
2024-05-25 08:50:21 +0400

    first commit

---
+++ Task/Fiorella/Helpers/Extensions/FileExtension.cs
@@ -0,0 +1,30 @@
+﻿namespace Fiorella.Helpers.Extensions
+{
+    public static class FileExtension
+    {
+        public static bool CheckFileSize(this IFormFile file, int size)
+        {
+            return file.Length / 1024 < size;
+        }
+
+
+        public static bool CheckFileType(this IFormFile file, string pattern)
+        {
+            return file.ContentType.Contains(pattern);
+        }
+
+        public async static Task SavaFileToLocalAsync(this IFormFile file, string path)
+        {
+            using FileStream fileStream = new(path, FileMode.Create);
+            await file.CopyToAsync(fileStream);
+        }
+
+        public static void DeleteFileFromLocal(this string path)
+        {
+
+            if (File.Exists(path))
+                File.Delete(path);
+        }
+
+    }
+}
